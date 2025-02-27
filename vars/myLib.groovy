0ac68e285879260ed36c4df1c16001383f059e0b
Gourav patel <softtech.gourav@gmail.com>
2025-02-27 14:10:46 +0530

    Update myLib.groovy

--- vars/myLib.groovy
+++ vars/myLib.groovy
@@ -1,4 +1,19 @@
-def call(){
-    sh 'docker login http://3.144.102.126:8090 -u admin -p Harbor12345'
-    sh 'docker pull 3.144.102.126:8090/tv/tv_app:latest'
+// vars/myLib.groovy
+def call() {
+    // Access environment variables
+    def dockerRegistry = env.DOCKER_REGISTRY
+    def dockerRepo = env.DOCKER_REPO
+    def imageName = "${dockerRegistry}/${dockerRepo}:latest"
+    def credentialsId = env.HARBOR_CREDENTIALS_ID
+
+    // Ensure all required environment variables are set
+    if (!dockerRegistry || !dockerRepo || !credentialsId) {
+        error "Required environment variables (DOCKER_REGISTRY, DOCKER_REPO, HARBOR_CREDENTIALS_ID) are not set."
+    }
+
+    // Perform Docker pull within the registry context
+    docker.withRegistry("http://${dockerRegistry}", credentialsId) {
+        def appImage = docker.image(imageName)
+        appImage.pull()
+    }
 }
