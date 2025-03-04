aa5bdebe72f075b0ef851ed9d135db6ea8435df4
Gourav patel <softtech.gourav@gmail.com>
2025-03-04 15:37:07 +0530

    Delete vars/gitClone.groovy

--- vars/gitClone.groovy
+++
@@ -1,9 +0,0 @@
-def call(String credentialsId, String repoUrl, String branch = 'master') {
-    withCredentials([string(credentialsId: credentialsId, variable: 'GIT_TOKEN']) {
-        // Construct the Git URL with the token
-        def fullRepoUrl = "https://${GIT_TOKEN}@${repoUrl}"
-        
-        // Clone the repository
-        git url: fullRepoUrl, branch: branch
-    }
-}
