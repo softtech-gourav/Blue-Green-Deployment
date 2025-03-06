5e1c503e4d5eb7940b44bee1506efbf51211bc3c
Gourav patel <softtech.gourav@gmail.com>
2025-03-06 14:18:28 +0530

    Update surveyGitClone.groovy

--- vars/surveyGitClone.groovy
+++ vars/surveyGitClone.groovy
@@ -3,8 +3,8 @@ def call() {
     def gitCredentialsId = env.GIT_CREDENTIALS_ID
 
     withCredentials([string(credentialsId: gitCredentialsId, variable: 'MY_GIT_TOKEN')]) {
-        def gitRepoUrl = "https://digvijaynath:${env.MY_GIT_TOKEN}@bitbucket.org/thppython/tv.git"
+        def gitRepoUrl = "https://digvijaynath:${env.MY_GIT_TOKEN}@bitbucket.org/thppython/survey.git"
 
-        git url: gitRepoUrl, branch: 'master'
+        git url: gitRepoUrl, branch: 'v1-mui-theme-survey-node-admin'
     }
 }
